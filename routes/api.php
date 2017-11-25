27207bae3b216f7be633132ef9d780269407e25d
孙传亮 <754442114@qq.com>
2017-11-25 10:00:28 +0800

    API资源路由

--- routes/api.php
+++ routes/api.php
@@ -24,3 +24,10 @@ Route::get('bar', function () {
 Route::get('users/{user}', function (App\User $user){
     dd($user);
 });
+
+//  API资源路由
+//  声明被 API 消费的资源路由时，
+//你可能需要排除展示 HTML 模板的路由，
+//如 create 和 edit，为了方便起见，
+//Laravel 提供了 apiResource 方法自动排除这两个路由：
+Route::apiResource('post', 'PostController');
